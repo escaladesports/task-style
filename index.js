9671b081ad8467ae1619c973b012a8cecb548771
Kennedy Rose <kennedy@kennedyrose.com>
2017-04-20 15:01:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict'
+const notify = require('task-notify')
+const error = require('task-error-notify')
+const gulp = require('gulp')
+const sourcemaps = require('gulp-sourcemaps')
+const sass = require('gulp-sass')
+const plumber = require('gulp-plumber')
+const filter = require('gulp-filter')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const assets = require('postcss-assets')
+const mqPacker = require('css-mqpacker')
+module.exports = (config, cb) => {
+	gulp.src(`${config.src}/*.scss`)
+		.pipe(plumber({ errorHandler: error }))
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compact',
+			include: [
+				'./node_modules'
+			]
+		}))
+		.pipe(postcss([
+			assets({
+				loadPaths: [
+					`./${config.dist}/`
+				],
+				basePath: 'src/',
+				cachebuster: true
+			}),
+			autoprefixer({
+				browsers: config.browsers
+			}),
+			mqPacker({ sort: true }),
+			cssnano()
+		]))
+		.pipe(sourcemaps.write('/'))
+		.pipe(gulp.dest(config.dist))
+		.on('end', () =>{
+			notify('Sass processed')
+			if(typeof cb === 'function') cb()
+		})
+}
